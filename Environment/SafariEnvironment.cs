c757c039ac760bd291249d5463d7f8d789dc4de4
Samuel M <76839403+Syntrice@users.noreply.github.com>
2024-11-13 14:38:39 +0000

    Refactor SafariEnvironment Change generic type IEnvironment to IEnvironmentAbility for clarity Add missing ability check when adding animal to environment

--- Environment/SafariEnvironment.cs
+++ Environment/SafariEnvironment.cs
@@ -3,7 +3,7 @@
 
 namespace NcSafari.Environment
 {
-    internal abstract class SafariEnvironment<IEnvironment>(int maxSpeed)
+    internal abstract class SafariEnvironment<IEnvironmentAbility>(int maxSpeed)
     {
         public int MaxSpeed { get; set; } = maxSpeed;
 
@@ -13,9 +13,16 @@ namespace NcSafari.Environment
 
         public void AddAnimalInEnvironment(Animal animal)
         {
+            // if animal does not implement the correct environment ability
+            if (animal is not IEnvironmentAbility)
+            {
+                Console.WriteLine("Animal cannot survive in this environment!");
+                return;
+            }
+
             if (animal.Speed > MaxSpeed)
             {
-                Console.WriteLine("Animal is moving faster than this envirnoment allows!");
+                Console.WriteLine("Animal has faster speed than this envirnoment allows!");
                 return;
             }
 
