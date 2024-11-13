d08f6b7511b8f2d3e685bcd7ade14cadfc1313c5
Samuel M <76839403+Syntrice@users.noreply.github.com>
2024-11-13 15:41:59 +0000

    Remove IEnvironment list from World for now

--- Environment/World.cs
+++ Environment/World.cs
@@ -5,9 +5,6 @@ namespace NcSafari.Environment
 {
     internal class World
     {
-
-        // a list of environments containing ITravel implementations
-        public List<IEnvironment> Environments { get; }
         public List<Animal> Animals { get; }
 
         public World()
@@ -22,22 +19,19 @@ namespace NcSafari.Environment
                   new Penguin(15.8f, 1)
                 };
 
-            Environments = new List<IEnvironment>();
-
             Water water = new Water();
             HuntingGround ground = new HuntingGround();
             Sky sky = new Sky();
 
             water.AddAnimalInEnvironment(Animals[1]);
             water.AddAnimalInEnvironment(Animals[5]);
+
             ground.AddAnimalInEnvironment(Animals[0]);
             ground.AddAnimalInEnvironment(Animals[4]);
+
             sky.AddAnimalInEnvironment(Animals[2]);
             sky.AddAnimalInEnvironment(Animals[3]);
 
-            Environments.Add(water);
-            Environments.Add(ground);
-            Environments.Add(sky);
         }
 
         public void Tick()
